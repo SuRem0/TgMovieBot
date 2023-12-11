06ceae9a63828421c94a074758db5cadc0f97bc1
SuRem0 <surentshik9000@gmail.com>
2023-12-11 23:28:54 +0300

    ya zaebalsya

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print(10)
+import g4f
+
+def get_answer(name):
+    response = g4f.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        provider=g4f.Provider.FakeGpthe,
+        messages=[{"role": "user", "content": name}],
+        stream=False,
+    )  
+    print(response)
+
+get_answer(str(input("введите запрос: ")))
