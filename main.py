a6f269869706cb34095e3e19f4876069a7b0fd83
SuRem0 <surentshik9000@gmail.com>
2023-12-16 13:34:55 +0300

    rabotaet ura

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ import g4f
 
 def get_answer(name):
     response = g4f.ChatCompletion.create(
-        model='gpt_35_long',
+        model=g4f.models.gpt_35_long,
         messages=[{"role": "user", "content": name}],
-        #proxy="http://162.248.225.227:80",
         stream=False,
-    )  
+    )
     print(response)
 
 while True:
     get_answer(str(input("введите запрос: ")))
-
-    #g4f.models.default,
