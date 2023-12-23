a9e4a620464198d8023e9a66072de76f08228604
SuRem0 <surentshik9000@gmail.com>
2023-12-24 00:36:29 +0300

    optimize, multi-user

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import telebot
 import main_module
 
 bot = telebot.TeleBot('6843284956:AAHBNPfGSoMJF1MQA0jtqcvVgfp-s18S4Lk')
-films = ""
+users = {}
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id,'Привет! Напиши название фильма, чтобы найти похожее')
     bot.register_next_step_handler(message, film)
 
 def film (message):
-    global films
-    films = message.text.lower()
+    global users
+    users[message.chat.id] = message.text.lower()
     markup = telebot.types.InlineKeyboardMarkup()
     btn1 = telebot.types.InlineKeyboardButton('Сюжет', callback_data='suzhet')
     btn2 = telebot.types.InlineKeyboardButton('Атмосфера', callback_data='atmos')
@@ -22,26 +23,19 @@ def film (message):
 
 @bot.callback_query_handler(func = lambda callback: True)
 def callback_massage(callback):
+    global users
+    bot.delete_message(callback.message.chat.id,callback.message.message_id)
+    bot.send_message(callback.message.chat.id, 'Подождите...')
     match callback.data:
         case 'suzhet': #Сюжет
-            bot.delete_message(callback.message.chat.id,callback.message.message_id)
-            bot.send_message(callback.message.chat.id, 'Подождите...')
-            bot.send_message(callback.message.chat.id, main_module.get_answer(films, "сюжет"), disable_web_page_preview=True)
+            bot.send_message(callback.message.chat.id, main_module.get_answer(users[callback.message.chat.id], "сюжет"), disable_web_page_preview=True)
         case 'atmos': #Атмосфера
-            bot.delete_message(callback.message.chat.id,callback.message.message_id)
-            bot.send_message(callback.message.chat.id, 'Подождите...')
-            bot.send_message(callback.message.chat.id, main_module.get_answer(films, "атмосфера"), disable_web_page_preview=True)
+            bot.send_message(callback.message.chat.id, main_module.get_answer(users[callback.message.chat.id], "атмосфера"), disable_web_page_preview=True)
         case 'ai': #Актерская игра
-            bot.delete_message(callback.message.chat.id,callback.message.message_id)
-            bot.send_message(callback.message.chat.id, 'Подождите...')
-            bot.send_message(callback.message.chat.id, main_module.get_answer(films, "актерская игра"), disable_web_page_preview=True)
+            bot.send_message(callback.message.chat.id, main_module.get_answer(users[callback.message.chat.id], "актерская игра"), disable_web_page_preview=True)
         case 'zhanr': #Жанр
-            bot.delete_message(callback.message.chat.id,callback.message.message_id)
-            bot.send_message(callback.message.chat.id, 'Подождите...')
-            bot.send_message(callback.message.chat.id, main_module.get_answer(films, "жанр"), disable_web_page_preview=True)
+            bot.send_message(callback.message.chat.id, main_module.get_answer(users[callback.message.chat.id], "жанр"), disable_web_page_preview=True)
         case 'spec': #Спецэфекты
-            bot.delete_message(callback.message.chat.id,callback.message.message_id)
-            bot.send_message(callback.message.chat.id, 'Подождите...')
-            bot.send_message(callback.message.chat.id, main_module.get_answer(films, "спецэфекты"), disable_web_page_preview=True)
+            bot.send_message(callback.message.chat.id, main_module.get_answer(users[callback.message.chat.id], "спецэфекты"), disable_web_page_preview=True)
     bot.send_message(callback.message.chat.id, 'Напишите /start , чтобы начать сначала')
 bot.infinity_polling()
